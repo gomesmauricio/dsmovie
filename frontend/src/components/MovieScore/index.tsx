6fae93b63b11f94f817847d420bae85d32ad8f0b
unknown <josemauricio.gomes@gmail.com>
2022-01-13 20:48:44 -0300

    MovieCard

---
+++ frontend/src/components/MovieScore/index.tsx
@@ -0,0 +1,18 @@
+import MovieStarts from "components/MovieStars";
+import './styles.css';
+
+const score = 3.5;
+const count = 13;
+
+function MovieScore() {
+
+    return (
+        <div className="dsmovie-score-container">
+            <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
+            <MovieStarts />
+            <p className="dsmovie-score-count">{count} avaliações</p>
+        </div>
+    )
+}
+
+export default MovieScore;
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,49 @@
-import Pagination from "components/Pagination";
-import './styles.css';
 
-function Listing(){
-    return(
-        <Pagination />
+import MovieCard from "components/MovieCard";
+import Pagination from "components/Pagination";
+
+function Listing() {
+    return (
+        <>
+            <Pagination />
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-2">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-2">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-2">
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
+            <br />
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-2">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-2">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-2">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
+            <br />
+        
+        </>
     )
 }
 
