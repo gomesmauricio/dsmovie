78fdbd8f96d7f946ce8d2fcddd537d6a00903714
unknown <josemauricio.gomes@gmail.com>
2022-01-13 22:53:44 -0300

    Domain model database-sid

---
+++ backend/dsmovie/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
@@ -0,0 +1,34 @@
+package com.devsuperior.dsmovie.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "tb_movie")
+public class Movie {
+	
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String title;
+	private Double score;
+	private Integer count;
+	private String image;
+
+}
---
+++ backend/dsmovie/src/main/java/com/devsuperior/dsmovie/entities/Score.java
@@ -0,0 +1,38 @@
+package com.devsuperior.dsmovie.entities;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "tb_score")
+public class Score {
+	
+	@Id
+	@EmbeddedId
+	@EqualsAndHashCode.Include
+	private ScorePK id = new ScorePK();
+	
+	private Double value;
+	
+	public void setMovie(Movie movie) {
+		id.setMovie(movie);
+	}
+	
+	public void seUser(User user) {
+		id.setUser(user);
+	}
+
+}
